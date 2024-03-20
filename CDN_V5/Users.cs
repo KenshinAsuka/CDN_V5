2eb16abd9caa7b1e15866253fe9a14bacb5b2a92
BillAng <kenshinasuka@gmail.com>
2024-03-21 06:46:10 +0800

    Initial commit

---
+++ CDN_V5/Users.cs
@@ -0,0 +1,15 @@
+﻿namespace CDN_V5
+{
+    public class Users
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; } = "";
+
+        public string PhoneNumber { get; set; } = "";
+
+        public string SkillSets { get; set; } = "";
+
+        public string Hobby { get; set; } = "";
+    }
+}
---
+++ CDN_V5/WebApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CDN_V5;
+
+namespace CDN_V5
+{
+    public class WebApiContext : DbContext
+    {
+        public WebApiContext(DbContextOptions<WebApiContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<CDN_V5.Users> Users { get; set; } = default!;
+    }
+}
